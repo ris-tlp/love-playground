adeb8be2593fc90f62b404b80b256f041e3eb945
ris-tlp <omarkhantlp@gmail.com>
2018-09-15 20:57:03 +0300

    added bullet functionality

--- TopdownShooter/main.lua
+++ TopdownShooter/main.lua
@@ -16,6 +16,8 @@ function love.load()
   zombies = {}
   bullets = {}
 
+  maxTime = 2
+  timer = maxTime
 end
 
 function love.update(dt)
@@ -36,6 +38,57 @@ function love.update(dt)
             end
         end
     end
+
+  for i,b in ipairs(bullets)
+    do
+      b.x = b.x + math.cos(b.direction) * b.speed * dt
+      b.y = b.y + math.sin(b.direction) * b.speed * dt
+    end
+
+  for i=#bullets,1,-1
+    do
+      local b = bullets[i]
+
+      if b.x < 0 or b.y < 0 or b.x > love.graphics.getWidth() or b.y > love.graphics.getHeight() or b.dead == true
+        then
+          table.remove(bullets, i)
+        end
+    end
+
+    for i,z in ipairs(zombies)
+      do
+        for j,b in ipairs(bullets)
+          do
+            if distance(z.x, z.y, b.x, b.y) < 20
+              then
+                z.dead = true
+                b.dead = true
+              end
+          end
+      end
+
+    for i,z in ipairs(zombies)
+      do
+        local z = zombies[i]
+
+        if z.dead == true
+          then
+            table.remove(zombies, i)
+          end
+      end
+
+      if gameState == 2
+        then
+          timer = timer - dt
+
+          if timer <= 0
+            then
+              spawnZombie()
+              maxTime = maxTime * 0.95
+              timer = maxTime
+            end
+        end
+
 end
 
 function love.draw()
@@ -47,6 +100,10 @@ function love.draw()
       love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(z), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
     end
 
+  for i,b in ipairs(bullets)
+    do
+      love.graphics.draw(sprites.bullet, b.x, b.y, b.direction, 0.3, 0.3, sprites.bullet:getWidth()/3, sprites.bullet:getHeight()/3)
+    end
 end
 
 function playerMovement(dt)
@@ -99,8 +156,25 @@ function spawnZombie()
       zombie.y = math.random(0,love.graphics.getHeight())
   end
 
-  table.insert(zombies,zombie)
+  table.insert(zombies, zombie)
+end
 
+function spawnBullet()
+  bullet = {}
+  bullet.x = player.x
+  bullet.y = player.y
+  bullet.speed = 500
+  bullet.direction = playerMouseAngle()
+  bullet.dead = false
+
+  table.insert(bullets, bullet)
+end
+
+function love.mousepressed(x, y, b, istouch)
+  if b == 1 and gameState == 2
+  then
+    spawnBullet()
+  end
 end
 
 function playerMouseAngle()
@@ -109,6 +183,13 @@ end
 
 function zombiePlayerAngle(enemy)
   return math.atan2(player.y - enemy.y, player.x - enemy.x)
+end
+
+function love.keypressed(key, scancode, isrepeat)
+  if key == "space"
+  then
+    spawnZombie()
+  end
 end
 
 function distance(x1, y1, x2, y2)
