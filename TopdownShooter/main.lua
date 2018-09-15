d06d656c3ad608a4af92addc61a4f98acc0eb1e4
ris-tlp <omarkhantlp@gmail.com>
2018-09-15 20:22:04 +0300

    added zombies and zombie movements

--- TopdownShooter/main.lua
+++ TopdownShooter/main.lua
@@ -1,5 +1,5 @@
 function love.load()
-  gameState = 1 --1 play --2 main-menu
+  gameState = 2 --1 main menu --2 play
 
   sprites = {}
   sprites.player = love.graphics.newImage('sprites/player.png')
@@ -22,11 +22,30 @@ function love.update(dt)
 
   playerMovement(dt)
 
+  for i,z in ipairs(zombies)
+    do
+      z.x = z.x + math.cos(zombiePlayerAngle(z)) * z.speed * dt
+      z.y = z.y + math.sin(zombiePlayerAngle(z)) * z.speed * dt
+
+      if distance(z.x, z.y, player.x, player.y) < 20
+        then
+          for i,z in ipairs(zombies)
+            do
+              zombies[i] = nil
+              gameState = 1
+            end
+        end
+    end
 end
 
 function love.draw()
   love.graphics.draw(sprites.background)
   love.graphics.draw(sprites.player, player.x, player.y, playerMouseAngle(), 1.2, 1.2, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+
+  for i,z in ipairs(zombies)
+    do
+      love.graphics.draw(sprites.zombie, z.x, z.y, zombiePlayerAngle(z), nil, nil, sprites.player:getWidth()/2, sprites.player:getHeight()/2)
+    end
 
 end
 
@@ -54,6 +73,44 @@ function playerMovement(dt)
 
 end
 
+function spawnZombie()
+
+  zombie = {}
+  zombie.x = 150
+  zombie.y = 150
+  zombie.speed = 130
+  zombie.dead = false
+  local side = math.random(1,4)  --1 up --2 right --3 down --4 left
+
+  if side == 1
+    then
+      zombie.x = math.random(0,love.graphics.getWidth())
+      zombie.y = -20
+  elseif side == 2
+    then
+      zombie.x = love.graphics.getWidth() + 20
+      zombie.y = math.random(0,love.graphics.getHeight())
+  elseif side == 3
+    then
+      zombie.x = math.random(0,love.graphics.getWidth())
+      zombie.y = love.graphics.getHeight() + 20
+  else
+      zombie.x = -20
+      zombie.y = math.random(0,love.graphics.getHeight())
+  end
+
+  table.insert(zombies,zombie)
+
+end
+
 function playerMouseAngle()
   return math.atan2(love.mouse.getY() - player.y, love.mouse.getX() - player.x)
+end
+
+function zombiePlayerAngle(enemy)
+  return math.atan2(player.y - enemy.y, player.x - enemy.x)
+end
+
+function distance(x1, y1, x2, y2)
+  return math.sqrt( (x2 - x1)^2 + (y2 - y1)^2)
 end
