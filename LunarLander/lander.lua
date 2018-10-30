3ea269dab29ce746bc62b4f9eeba59577067da7a
ris-tlp <omarkhantlp@gmail.com>
2018-10-30 23:41:40 +0300

    added lander properties and basic movement

---
+++ LunarLander/lander.lua
@@ -0,0 +1,7 @@
+lander = {}
+lander.x = 300
+lander.y = 100
+lander.speed = 180
+lander.angle = 0
+lander.fuel = 100
+lander.exploded = false
--- LunarLander/main.lua
+++ LunarLander/main.lua
@@ -1,5 +1,6 @@
 function love.load()
 
+  require('lander')
   love.window.setMode(640, 480, {vsync = false})
 
   sprites = {}
@@ -12,12 +13,33 @@ end
 
 
 function love.update(dt)
-
+  playerMovement(dt)
 end
 
 
 function love.draw()
-
   love.graphics.draw(sprites.background, 0, 0)
+  love.graphics.draw(sprites.lander, lander.x, lander.y)
+end
 
+function playerMovement(dt)
+  if love.keyboard.isDown("s") and lander.y < love.graphics.getHeight()
+        then
+          lander.y = lander.y + lander.speed * dt
+        end
+
+  if love.keyboard.isDown("w") and lander.y > 0
+        then
+          lander.y = lander.y - lander.speed * dt
+        end
+
+  if love.keyboard.isDown("d") and lander.x < love.graphics.getWidth()
+        then
+          lander.x = lander.x + lander.speed * dt
+        end
+
+  if love.keyboard.isDown("a") and lander.x > 0
+        then
+          lander.x = lander.x - lander.speed * dt
+        end
 end
